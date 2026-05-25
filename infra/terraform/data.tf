c23d0fa54847dbe90ed32a5db7768cf0233c04d6
VFull <vfull@DESKTOP-VICTOR>
2026-05-25 11:41:15 -0500

    Fix S3 bucket policy to avoid invalid resource ARNs (artifacts only)

--- infra/terraform/data.tf
+++ infra/terraform/data.tf
@@ -6,8 +6,6 @@ data "aws_iam_policy_document" "bucket_tls_only" {
     effect  = "Deny"
     actions = ["s3:*"]
     resources = [
-      "${aws_s3_bucket.log_archive.arn}",
-      "${aws_s3_bucket.log_archive.arn}/*",
       "${aws_s3_bucket.artifacts.arn}",
       "${aws_s3_bucket.artifacts.arn}/*",
     ]
